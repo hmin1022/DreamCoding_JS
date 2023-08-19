c78d4ffb2318aab8a9ff5651b148c4bc01e2d271
hmin1022 <leehyeonsnail@gmail.com>
2023-08-19 20:21:28 +0900

    Feat: 연산자 실습

---
+++ 2.arithmetic.js
@@ -0,0 +1,20 @@
+// 산술연산자 (Arithmetic operators)
+// + 더하기
+// - 빼기
+// * 곱하기
+// / 나누기
+// % 나머지
+// ** 지수
+
+console.log(5 + 2);
+console.log(5 - 2);
+console.log(5 * 2);
+console.log(5 / 2);
+console.log(5 % 2);
+console.log(5 ** 2);
+
+// + 연산자 주의점
+let text = '두개의' + '문자를';
+console.log(text);
+text = 1 + '1';
+console.log(text);
