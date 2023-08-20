2c75dc798620072653b8e60982dbb99df0452bfa
hmin1022 <leehyeonsnail@gmail.com>
2023-08-20 23:07:11 +0900

    Feat: 삼항조건 연산자 실습

---
+++ 2.ternary.js
@@ -0,0 +1,7 @@
+// 삼항 조건 연산자 Ternary Operator
+// 조건식 ? 참인경우 : 거짓인경우
+let fruit = 'apple';
+fruit === 'apple' ? console.log('🍎') : console.log('🍊');
+
+let orange = fruit === 'orange' ? '🍎' : '🍊';
+console.log(orange);
