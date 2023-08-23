2d94a4feb2bbbd65c492971180e879c1a552d8dc
hmin1022 <leehyeonsnail@gmail.com>
2023-08-23 23:20:07 +0900

    Feat: function 실습

---
+++ 1.basic.js
@@ -0,0 +1,14 @@
+// 사용예제 1
+function add(a, b) {
+  return a + b;
+}
+const result = add(1, 2);
+console.log(result);
+
+// 사용예제 2
+function fullName (firstName, lastName) {
+return `${firstName} ${lastName}`;
+}
+
+console.log(fullName('김', '지수'));
+console.log(fullName('이', '철수'));
