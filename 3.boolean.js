84fee1655c44630d200e0eb794d0c352509eb73a
hmin1022 <leehyeonsnail@gmail.com>
2023-09-05 13:56:31 +0900

    Feat: boolean 복습

---
+++ 3.boolean.js
@@ -0,0 +1,23 @@
+// Boolean
+const isTrue = new Boolean(true);
+console.log(isTrue.valueOf());
+
+/**
+ * Falshy
+ * NaN
+ * undefined
+ * ''
+ * 0
+ * -0
+ * null
+ */
+
+/**
+ * Truthy
+ * 1
+ * -1
+ * '0'
+ * 'false'
+ * []
+ * {}
+ */
