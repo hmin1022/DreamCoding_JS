4428b4190a3d50a9989a78850d24f0280fa6f117
hmin1022 <leehyeonsnail@gmail.com>
2023-09-12 23:55:38 +0900

    Feat: high order function 실습

--- 6.hof.js
+++ 6.hof.js
@@ -21,3 +21,14 @@ console.log(found);
 // 배열의 아이템들이 부분적으로 조건(콜백함수)에 맞는지 확인
 result = products.some((item) => item.name === '🍪');
 console.log(result);
+
+// 배열 아이템들이 부분적으로 조건(콜백함수)에 맞는지 확인
+result = products.every((item) => item.name === '🍪');
+console.log(result);
+
+// 조건에 맞는 모든 아이템들을 새로운 배열로!
+result = products.filter((item) => item.name === '🍪');
+console.log(result);
+
+// Map 배열의 아이템들을 다른 아이템으로 매핑할 수 있는, 변환해서 새로운 배열 생성
+const noms = []
