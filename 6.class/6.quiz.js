4179f84a5761bd125aeca23ad9284bf46e41516f
hmin1022 <leehyeonsnail@gmail.com>
2023-09-25 18:07:01 +0900

    Docs: 변수 수정

--- 6.class/6.quiz.js
+++ 6.class/6.quiz.js
@@ -2,7 +2,7 @@
 // 0이상의 값으로 초기화 한 뒤 하나씩 숫자를 증가할 수 있는 카운터를 만들기
 // Counter 클래스 만들기
 class Counter {
-  constructor(value) {
+  constructor(startValue) {
     if(isNaN(startValue) || startValue < 0) {
       this.#value = 0;
     } else {
