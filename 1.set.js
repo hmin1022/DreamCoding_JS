8d95e2b4d0353515b772e914b4ebcd2c4489042c
hmin1022 <leehyeonsnail@gmail.com>
2023-10-06 10:35:31 +0900

    Feat: set 실습

---
+++ 1.set.js
@@ -0,0 +1,48 @@
+// Set
+const set = new Set([1, 2, 3]);
+console.log(set);
+
+// 사이즈
+console.log(set.size);
+
+// 존재여부
+console.log(set.has(2));
+console.log(set.has(6));
+
+// 순회
+set.forEach((item) => console.log(item));
+for(const value of set.values()) {
+  console.log(value);
+}
+
+// 추가
+set.add(6);
+console.log(set);
+set.add(6); // 중복 불가
+console.log(set);
+
+// 삭제
+set.delete(6);
+console.log(set);
+
+// 전부 삭제
+set.clear();
+console.log(set);
+
+// object set
+const obj1 = { name: '🍎', price: 8 };
+const obj2 = { name: '🍌', price: 5 };
+const objs = new Set([obj1, obj2]);
+console.log(objs);
+
+// 퀴즈 shallow coppy
+obj1.price = 10;
+objs.add(obj1);
+console.log(objs);
+
+// 퀴즈
+const obj3 = { name: '🍌', price: 5 };
+objs.add(obj3);
+console.log(objs);
+obj3.price = 8;
+console.log(objs);
