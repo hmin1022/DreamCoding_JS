6ac074d0c8fc9173b0e6c7be34bb028e4caf0ef3
hmin1022 <leehyeonsnail@gmail.com>
2023-10-06 15:52:03 +0900

    Feat: nullish 실습

--- 3.nullish.js
+++ 3.nullish.js
@@ -4,5 +4,5 @@
 // || falshy한 경우 설정(할당)
 let num = 0;
 console.log(num || '-1'); // num의 0값이 falshy로 간주되어 버그가 발생할수도 있다.
-// 그런경우 ??를 사용한다
+// 그런경우 ??를 사용하여 null, undefined만 falshy값으로 분별할 수 있게 만듬
 console.log(num ?? '-1');
